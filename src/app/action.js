3bf4f00d1e57471a63dbcdf718f71111bc8f44c0
luqmaaaaann <luqmanhakim396@gmail.com>
2025-06-30 15:51:44 +0700

    add session

--- src/app/action.js
+++ src/app/action.js
@@ -1,12 +1,105 @@
 "use server";
 
-import { createNote } from "@/services/notes";
-const { revalidatePath } = require("next/cache");
+import { getSession } from "@/services/session";
+import { prisma } from "@/utils/prisma";
+import bcrypt from "bcrypt";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export async function createTodoAction(formData) {
-  const title = formData.get("title");
-  const content = formData.get("content");
+export async function registerAction(formData) {
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-  await createNote(title, content);
-  revalidatePath("/");
+  console.log({ name, email, password });
+
+  try {
+    await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+  } catch (err) {
+    console.log(err);
+    return {
+      message: "User already exist",
+    };
+  }
+}
+
+export async function loginAction(formData) {
+  const cookiesStore = await cookies();
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let user = null;
+
+  try {
+    user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+  } catch (err) {
+    console.log("Something went wrong");
+    return {
+      message: "Something went wrong",
+    };
+  }
+
+  if (!user) {
+    console.log("User does not exist");
+    return {
+      message: "User does not exist",
+    };
+  }
+
+  //cek Password yg benar/salah
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    console.log("Invalid Password");
+    return {
+      message: "Invalid Password",
+    };
+  }
+
+  //Authorization
+  const newSession = await prisma.session.create({
+    data: {
+      userId: user.id,
+    },
+  });
+
+  console.log({ newSession });
+
+  cookiesStore.set("sessionId", newSession.id, {
+    httpOnly: true,
+    maxAge: 1000 * 30,
+  });
+  redirect("/dashboard");
+}
+
+export async function logoutAction(_) {
+  const cookiesStore = await cookies();
+  const session = await getSession();
+
+  await prisma.session.delete({
+    where: {
+      id: session.id,
+    },
+  });
+
+  cookiesStore.delete("sessionId");
+  redirect("/");
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,63 @@
-import { prisma } from "@/utils/prisma";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { createTodoAction } from "./action";
-//Pemanggilan semua di noteMethods (generik)
-import * as noteMethods from "@/services/notes";
+import { loginAction, registerAction } from "./action";
 
 export default async function Home() {
-  //Pemanggilan getNotes ke services
-  const notes = await noteMethods.getAll();
+  //Pemanggilan semua data query ke database
+  // const users = await prisma.user.findMany({
+  //   //Mengambil relasi dari post milik user untuk mengambil id dan title
+  //   include: {
+  //     post: {
+  //       select: {
+  //         id: true,
+  //         title: true,
+  //       },
+  //     },
+  //   },
+  // });
 
+  //Render UI
   return (
     <div>
-      <form action={createTodoAction}>
-        <Input name="title" />
-        <Textarea name="content" />
-        <Button>Create</Button>
+      <form action={loginAction}>
+        <Input name="email" placeholder="Email" type="email" />
+        <Input name="password" placeholder="Password" type="password" />
+        <Button>Login</Button>
       </form>
-      <h3>My Notes</h3>
-      <div>
-        {/* melakukan mapping u/ pemanggilan data id dan title */}
-        {notes.map((note) => {
-          return <div key={note.id}>{note.title}</div>;
-        })}
-      </div>
     </div>
+
+    // <div>
+    //   <form action={registerAction}>
+    //     <Input name="name" placeholder="Name" />
+    //     <Input name="email" placeholder="Email" />
+    //     <Input name="password" placeholder="Password" type="password" />
+    //     <Button>Register</Button>
+    //   </form>
+    // </div>
+
+    // <div>
+    //   <h3>My users</h3>
+    //   <div>
+    //     {/* melakukan mapping u/ pemanggilan data id dan title */}
+    //     {users.map((user) => {
+    //       return (
+    //         <div key={user.id} className="p-4 rounded-lg w-fit border">
+    //           <div>
+    //             {user.name} - {user.email}
+    //           </div>
+    //           {/*jumlah post yg dimiliki user*/}
+    //           <div>User have {user.post.length} post</div>
+
+    //           {/*Nested Mapping (menampilkan semua judul post dari user)*/}
+    //           <div>
+    //             {user.post.map((post) => {
+    //               return <div key={post.id}>{post.title}</div>;
+    //             })}
+    //           </div>
+    //         </div>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
   );
 }
